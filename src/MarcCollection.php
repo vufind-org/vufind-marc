65bf9c620a79915f1ba85f1f36e37eef43416e69
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-11-12 12:14:44 -0500

    Undo Rector Changes

--- src/MarcCollection.php
+++ src/MarcCollection.php
@@ -87,7 +87,7 @@ class MarcCollection implements \Iterator
     {
         $this->position = 0;
         $this->records = [];
-        if ($data === '' || $data === '0') {
+        if (!$data) {
             return;
         }
         $valid = false;
