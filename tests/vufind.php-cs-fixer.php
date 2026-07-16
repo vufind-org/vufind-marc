9a28d525770e3fd5a2038f2785a8bf1148d1d3e9
Susan Turkel <sturkel@villanova.edu>
2026-07-16 13:33:35 -0400

    Updated  section of tests/vufind.php-cs-fixer.php

--- tests/vufind.php-cs-fixer.php
+++ tests/vufind.php-cs-fixer.php
@@ -5,16 +5,16 @@ $finder->in(__DIR__)
     ->in(__DIR__ . '/../src');
 
 $rules = [
-    '@PHP81Migration' => true,
-    '@PHPUnit100Migration:risky' => true,
+    '@PHP8x2Migration' => true,
     '@PSR12' => true,
     'align_multiline_comment' => true,
     'binary_operator_spaces' => [
         'default' => 'single_space',
-        'operators' => ['=' => null, '=>' => null],
     ],
+    'blank_line_after_opening_tag' => false,
     'cast_spaces' => ['space' => 'none'],
     'class_attributes_separation' => ['elements' => ['method' => 'one', 'property' => 'one']],
+    'class_reference_name_casing' => true,
     'concat_space' => ['spacing' => 'one'],
     'ereg_to_preg' => true,
     'get_class_to_class_keyword' => true,
@@ -23,7 +23,6 @@ $rules = [
         'import_classes' => null,
     ],
     'is_null' => true,
-    'linebreak_after_opening_tag' => true,
     'lowercase_cast' => true,
     'magic_constant_casing' => true,
     'modernize_strpos' => true,
@@ -44,6 +43,7 @@ $rules = [
     'no_php4_constructor' => true,
     'no_singleline_whitespace_before_semicolons' => true,
     'no_spaces_around_offset' => true,
+    'no_trailing_whitespace_in_comment' => false, // disabled for readability; we want < ?php // comment ? > not < ?php //comment? >
     'no_unneeded_braces' => true,
     'no_unneeded_control_parentheses' => true,
     'no_unneeded_final_method' => true,
@@ -57,11 +57,11 @@ $rules = [
         'sort_algorithm' => 'alpha',
     ],
     'phpdoc_no_access' => true,
-    'php_unit_method_casing' => true,
     'pow_to_exponentiation' => true,
     'single_line_after_imports' => true,
     'single_quote' => true,
     'standardize_not_equals' => true,
+    'statement_indentation' => false, // disabled because it messes up indentation of php tags
     'ternary_operator_spaces' => true,
     'type_declaration_spaces' => true,
 ];
@@ -72,7 +72,7 @@ if (!is_dir($cacheDir)) {
 }
 
 $config = new PhpCsFixer\Config();
-return $config->setCacheFile($cacheDir . '/.code.cache')
+return $config->setCacheFile($cacheDir . '/.template.cache')
     ->setRiskyAllowed(true)
     ->setRules($rules)
     ->setFinder($finder)
